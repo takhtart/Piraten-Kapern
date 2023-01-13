2bbeacc01b86746bd0962811399d5af0db0c568c
Tarnveer Takhtar <80164022+takhtart@users.noreply.github.com>
2023-01-13 13:58:08 -0500

    Completed F01, F02 (README.md For Details)

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,6 +1,18 @@
 import pk.Dice;
+import pk.Faces;
+
+import java.util.ArrayList;
 
 public class PiratenKarpen {
+
+    public static ArrayList<String> Roll8(){
+        ArrayList<String> Rolls = new ArrayList<String>();
+        Dice myDice = new Dice();
+        for (int i = 0; i <= 7; i++) {
+            Rolls.add(myDice.roll().toString());
+        }
+        return Rolls;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
@@ -8,6 +20,9 @@ public class PiratenKarpen {
         Dice myDice = new Dice();
         System.out.println(myDice.roll());
         System.out.println("That's all folks!");
+        System.out.println("I'm rolling 8 dice");
+        ArrayList<String> Rolls = Roll8();
+        System.out.println(Rolls);
     }
     
 }
