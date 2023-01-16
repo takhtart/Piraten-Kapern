2c76d6c11cae9c80c3c6f8964881d5e7007a5c47
Tarnveer Takhtar <80164022+takhtart@users.noreply.github.com>
2023-01-16 10:17:08 -0500

    Simulates Turns Of Both Players w/ the same Strat

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,28 +1,42 @@
 import pk.Dice;
 import pk.Faces;
+import pk.Player;
 
 import java.util.ArrayList;
 
 public class PiratenKarpen {
 
-    public static ArrayList<String> Roll8(){
-        ArrayList<String> Rolls = new ArrayList<String>();
-        Dice myDice = new Dice();
-        for (int i = 0; i <= 7; i++) {
-            Rolls.add(myDice.roll().toString());
-        }
-        return Rolls;
-    }
-
     public static void main(String[] args) {
+        Player Player1 = new Player();
+        Player Player2 = new Player();
+
         System.out.println("Welcome to Piraten Karpen Simulator!");
-        System.out.println("I'm rolling a dice");
-        Dice myDice = new Dice();
-        System.out.println(myDice.roll());
-        System.out.println("That's all folks!");
-        System.out.println("I'm rolling 8 dice");
-        ArrayList<String> Rolls = Roll8();
-        System.out.println(Rolls);
+
+        //Base Scores
+        System.out.println("Player 1 Score: " + Player1.getscore());
+        System.out.println("Player 1 Skulls: " + Player1.getskulls());
+        System.out.println("Player 2 Score: " + Player1.getscore());
+        System.out.println("Player 2 Skulls: " + Player1.getskulls());
+
+        //Player 1 Turn
+        System.out.println("Player 1: I'm rolling dice!");
+        Player1.Roll();
+        System.out.println("Player 1 Rolled: " + Player1.getroll());
+        System.out.println("Player 1 ReRolling!");
+        Player1.ReRoll();
+        System.out.println("Player 1 Final Roll: " + Player1.getroll());
+        System.out.println("Player 1 Score: " + Player1.getscore());
+
+        //Player 2 Turn
+        System.out.println("Player 2: I'm rolling dice!");
+        Player2.Roll();
+        System.out.println("Player 2 Rolled: " + Player2.getroll());
+        System.out.println("Player 2 ReRolling!");
+        Player2.ReRoll();
+        System.out.println("Player 2 Final Roll: " + Player2.getroll());
+        System.out.println("Player 2 Score: " + Player2.getscore());
+
+
     }
     
 }
